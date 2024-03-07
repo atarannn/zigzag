8e0e282c00bdc5c80c90b376d47de4e2ce0a1091
atarannn <anastasiatarannn@icloud.com>
2024-03-08 01:59:22 +0200

    style changes

--- src/assets/scripts/libs/libs.js
+++ src/assets/scripts/libs/libs.js
@@ -1,4 +1,3 @@
 @@include('../../../../node_modules/bezier-easing/dist/bezier-easing.min.js')
 @@include('./jquery/jquery.min.js')
 @@include('./swiper/swiper.js')
-@@include('./gsap/ScrollTrigger.min.js')
